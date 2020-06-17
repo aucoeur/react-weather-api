817a42770e7309de2cd733f81b11e1b9d457ed29
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-06-17 15:01:56 -0700

    Challenge 3

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 function Temperature(props) {
     const { temp, temp_min, temp_max}  = props
+    function convertF(temp) {
+        return (temp * (9/5) - 459.67).toFixed(2)
+    }
+
     return (
         <div>
-            <div>Temp: {temp}</div>
-            <div>Temp Min: {temp_min} Max:{temp_max}</div>
+            <div>Temp: {convertF(temp)}° F</div>
+            <div>Temp Min: {convertF(temp_min)}° F Max: {convertF(temp_max)}° F</div>
         </div>
     )
 }
